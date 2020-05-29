945527bcc07e85738a25122fd4861eb14a4724fb
WrightJerrod <wrightjerrod@gmail.com>
2020-05-29 12:14:56 -0500

    Began work on timer class.

--- testing.py
+++ testing.py
@@ -6,24 +6,23 @@ class DummyMember:
         self.display_name = name
 
 
-list1 = ["0000" + str(each) for each in range(1,10)]
-list2 = ["000" + str(each) for each in range(10,100)]
-list3 = ["00" + str(each) for each in range(100,1000)]
-list4 = ["01000", "01001", "01002"]
+list1 = ["0000" + str(each) for each in range(1, 10)]
+list2 = ["00" + str(each) for each in range(10, 100)]
+list3 = ["0" + str(each) for each in range(100, 1000)]
+list4 = ["1000", "1001", "1002"]
 list_of_digits = list1 + list2 + list3 + list4
 
 dummy_member = DummyMember("Username")
 
-list_of_participants = ["Username" + list_of_digits[each] for each in range(0, 101)]
-print(len(str([y for y in list_of_participants])))
-print(len(list_of_participants))
-#print(list_of_participants)
-
-list_of_lists = split_lists(list_of_participants, 400)
-
-for each in list_of_lists:
-    print(len(str([y for y in each])))
-    print(str([y for y in each]))
-
+# list_of_participants = ["Username" + list_of_digits[each] for each in range(0, 101)]
+# print(len(str([y for y in list_of_participants])))
+# print(len(list_of_participants))
+# #print(list_of_participants)
+#
+# list_of_lists = split_lists(list_of_participants, 400)
+#
+# for each in list_of_lists:
+#     print(len(str([y for y in each])))
+#     print(str([y for y in each]))
 
 
